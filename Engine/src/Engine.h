04f6e4966cd3e268fbdebc27c988b43179614087
edwards.cole97@gmail.com <edwards.cole97@gmail.com>
2020-09-25 13:29:48 -0700

    Setup Entry Point for Engine and Client Application

---
+++ Engine/src/Engine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// General includes
+
+
+// For use by Engine applications only
+#include "Engine/Application.h"
+
+// ENTRY POINT ----------------------------------
+#include "Engine/EntryPoint.h"
+// ----------------------------------------------
---
+++ Engine/src/Engine/EntryPoint.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef ENG_PLATFORM_WINDOWS
+
+extern Engine::Application* Engine::createApplication();
+
+int main(int argc, char** argv) {
+	auto app = Engine::createApplication();
+	app->run();
+	delete app;
+}
+
+#endif // ENG_PLATFORM_WINDOWS
